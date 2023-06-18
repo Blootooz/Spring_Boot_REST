c468a7d77d0f63af6f64c113ac27d4a2ffb77fb6
Alexandra <84934027+Blootooz@users.noreply.github.com>
2023-06-18 20:05:18 +0300

    Initial commit

---
+++ src/main/java/ru/netology/javaclo_2_spring_boot_rest/Java2SpringBootRestApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.javaclo_2_spring_boot_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java2SpringBootRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java2SpringBootRestApplication.class, args);
+    }
+
+}
